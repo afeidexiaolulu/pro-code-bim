c382a5d1f97d12e5ad6806bc68edfa632fd8af68
liangwenjie <lwjemail@163.com>
2019-06-28 14:51:48 +0800

    Initial commit

---
+++ bim-bean/src/main/java/com/zy/bim/bean.java
@@ -0,0 +1,14 @@
+package com.zy.bim;
+
+import lombok.Data;
+
+/**
+ * @author Liang Wenjie
+ * @version 1.00
+ * @time 2019/6/28 0028 下午 2:27
+ */
+@Data
+public class bean {
+
+    private Integer id;
+}
---
+++ bim-web/src/main/java/com/zy/bimweb/BimWebApplication.java
@@ -0,0 +1,13 @@
+package com.zy.bimweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BimWebApplication.class, args);
+    }
+
+}
